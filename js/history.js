2ddf32e84f35e2a0aed6ef52ec9f99943b9bf2b3
tobinchisholm-svg <tobin.chisholm@gmail.com>
2026-06-16 14:09:08 -0300

    added old church picutre to history page

--- js/history.js
+++ js/history.js
@@ -3,7 +3,6 @@
 // =============================================
 const IMAGES = {
     oldChurch:   'images/old-church.png',
-    oldInterior: 'images/old-interior.png',
 };
 
 // =============================================
